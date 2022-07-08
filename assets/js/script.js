523703e68a419d82f956a642bb8c052446b76fd8
Rayncor <sowa332@gmail.com>
2022-07-08 11:07:05 -0700

    added autocomplete function for breeds

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const dogBreedAPIKey = "5982d498-0ef4-4fe3-96b7-a547ee0abaf1"
 
 var dogBreed = 'https://dog.ceo/api/breeds/list/all'
 var imgBreed = 'https://dog.ceo/api/breed/hound/images'
+var dogBreeds = ['affenpinscher', 'african', 'airendale', 'akita', 'appenzeller', 'australian', 'basenji', 'beagle', 'bluetick', 'borzoi', 'bouvier', 'boxer', 'brabancon', 'briard', 'buhund', 'bulldog', 'bullterrier', 'cattledog', 'chihuahua', 'chow', 'clumber', 'cockapoo', 'collie', 'coonhound', 'corgi', 'cotondetulear', 'dachshund', 'dalmation', 'dane', 'deerhound', 'dhole', 'dingo', 'doberman', 'elkhound', 'entlebucher', 'eskimo', 'finnish', 'frise', 'germanshepherd', 'greyhound', 'groenendael', 'havanese', 'hound', 'husky', 'keeshond', 'kelpie', 'komondor', 'kuvasz', 'labradoodle', 'labrador', 'leonberg', 'lhasa', 'malamute', 'malinois', 'maltese', 'mastiff', 'mexicanhairless', 'mix', 'mountain', 'newfoundland', 'otterhound', 'ovcharka', 'papillon', 'pekinese', 'pembroke', 'pinscher', 'pitbull', 'pointer', 'pomeranian', 'poodle', 'pug', 'puggle', 'pyrenees', 'redbone', 'retriever', 'ridgeback', 'rottweiler', 'saluki', 'samoyed', 'schipperke', 'schnauzer', 'setter', 'sharpei', 'sheepdog', 'shiba', 'shihtzu', 'spaniel', 'springer', 'stbernard', 'terrier', 'tervuren', 'vizsla', 'waterdog', 'weimaraner', 'whippet', 'wolfhound'];
 
 // function getParkApi() {
 // 	let queryURL = 'https://data.seattle.gov/resource/2cer-njie.json'
@@ -50,3 +51,71 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 // getParkApi()
+
+function autocomplete(inp, arr) {
+	var currentFocus;
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value;
+		closeAllLists();
+		if (!val) { return false;}
+		currentFocus = -1;
+		a = document.createElement("div");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		this.parentNode.appendChild(a);
+		for (i = 0; i < arr.length; i++) {
+		  if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+			b = document.createElement("div");
+			b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+			b.innerHTML += arr[i].substr(val.length);
+			b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				b.addEventListener("click", function(e) {
+				inp.value = this.getElementsByTagName("input")[0].value;
+				closeAllLists();
+			});
+			a.appendChild(b);
+		  }
+		}
+	});
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+		  currentFocus++;
+		  addActive(x);
+		} else if (e.keyCode == 38) {
+		  currentFocus--;
+		  addActive(x);
+		} else if (e.keyCode == 13) {
+		  e.preventDefault();
+		  if (currentFocus > -1) {
+			if (x) x[currentFocus].click();
+		  }
+		}
+	});
+	function addActive(x) {
+	  if (!x) return false;
+	  removeActive(x);
+	  if (currentFocus >= x.length) currentFocus = 0;
+	  if (currentFocus < 0) currentFocus = (x.length - 1);
+	  x[currentFocus].classList.add("autocomplete-active");
+	}
+	function removeActive(x) {
+	  for (var i = 0; i < x.length; i++) {
+		x[i].classList.remove("autocomplete-active");
+	  }
+	}
+	function closeAllLists(elmnt) {
+	  var x = document.getElementsByClassName("autocomplete-items");
+	  for (var i = 0; i < x.length; i++) {
+		if (elmnt != x[i] && elmnt != inp) {
+		x[i].parentNode.removeChild(x[i]);
+	  }
+	}
+  }
+  document.addEventListener("click", function (e) {
+	  closeAllLists(e.target);
+  });
+  }
+
+  autocomplete(document.getElementById("breedInput"), dogBreeds);
