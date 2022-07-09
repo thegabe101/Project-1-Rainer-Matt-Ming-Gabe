09563db2db6b8fab077b289193784aeba74b681c
Hoefer <ningboninja@gmail.com>
2022-07-08 19:59:50 -0700

    adding pseudo code of fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,21 +122,60 @@ var dogBreeds = [
 // }
 
 // getParkApi()
+//TODO: Incorporate this somehow. 
+// var tableBody = document.getElementById('repo-table');
+// var fetchButton = document.getElementById('fetch-button');
+
+// function getApi() {
+//     // fetch request gets a list of all the repos for the node.js organization
+//     var postmanAPIURL = 'https://api.thedogapi.com/v1/breeds'
+
+//     fetch(postmanAPIURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//             //TODO: if statement
+
+//             //Loop over the data to generate a table, each table row will have a link to the repo url
+//             for (var i = 0; i < data.length; i++) {
+//                 // Creating elements, tablerow, tabledata, and anchor
+//                 var createTableRow = document.createElement('tr');
+//                 var tableData = document.createElement('td');
+//                 var link = document.createElement('a');
+
+//                 // Setting the text of link and the href of the link
+//                 link.textContent = data[i].breed_group + " " + data[i].name + " " + data[i].life_span
+//                 // when Links get clicked on you go to results and the dog name = breedInput
+//                 //TODO: 
+//                 link.href = "https://en.wikipedia.org/wiki/" + data[i].name;
+
+//                 // Appending the link to the tabledata and then appending the tabledata to the tablerow
+//                 // The tablerow then gets appended to the tablebody
+//                 tableData.appendChild(link);
+//                 createTableRow.appendChild(tableData);
+//                 tableBody.appendChild(createTableRow);
+//             }
+//         });
+// }
+
+fetchButton.addEventListener('click', getApi);
 
 const contentTag = document.getElementById('breedDoggo');
 fetch(postmanAPIURL)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
+	.then(res => res.json())
+	.then(data => {
+		console.log(data)
 
-    const jsonToString = JSON.stringify(data[1].breed_group);
-    console.log('jsonToString', jsonToString);
+		const jsonToString = JSON.stringify(data[1].breed_group);
+		console.log('jsonToString', jsonToString);
 
-    contentTag.textContent = `
+		contentTag.textContent = `
     You have chosen a ${data[1].breed_group}!
     `;
-    console.log("name", data[1].breed_group);
-  });
+		console.log("name", data[1].breed_group);
+	});
 
 var options
 document.addEventListener('DOMContentLoaded', function () {
@@ -301,8 +340,6 @@ document.getElementById('wikiDogBtn').addEventListener('click', function () {
 		document.getElementById('wikiDogBtn').appendChild(para)
 	}
 })
-
-
 
 //TODO: take user input on breed group 
 //TODO: fetch data
